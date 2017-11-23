1f2bab487a447122b35034dd2b438b723d914902
Student <student@setass-mac-mini-41.local>
2017-11-22 20:45:06 -0500

    created table view with each cell selectable

--- SimpleTable/ViewController.swift
+++ SimpleTable/ViewController.swift
@@ -1,25 +1,71 @@
-//
-//  ViewController.swift
-//  SimpleTable
-//
-//  Created by Student on 2017-11-22.
-//  Copyright © 2017 Centennial College. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    private let movies = ["Batman vs Superman","Blade Runner 2049","Thor: Ragnorok","Justice League","Wonderwoman","Dark Tower","Star Wars: The Last Jedi"]
+    
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        
+        if(cell == nil){
+            
+            
+            cell = UITableViewCell(
+            
+                style: UITableViewCellStyle.value1,reuseIdentifier:simpleTableIdentifier)}
+        
+        let image = UIImage(named: "star")
+        cell?.imageView?.image = image
+        let highlightedImage = UIImage(named: "star2")
+       cell?.imageView?.highlightedImage = highlightedImage
+        
+        
+        if indexPath.row < 4 {
+            cell?.detailTextLabel?.text = "SuperHero"
+        }else{
+            cell?.detailTextLabel?.text = "Sci-Fi"
+        }
+        
+        cell?.textLabel?.text = movies[indexPath.row]
+        return cell!
     }
-
-
+ 
+    
+    func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
+        return indexPath.row % 4
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return indexPath.row == 0 ? nil : indexPath
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let rowValue = movies[indexPath.row]
+        let message  = "You Selected \(rowValue)"
+        
+        let controller = UIAlertController(title: "Row selected", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Yes I did", style: .default, handler: nil)
+        
+        controller.addAction(action)
+        
+        present(controller,animated: true,completion: nil)
+    }
+    
 }
 
+    
+    
